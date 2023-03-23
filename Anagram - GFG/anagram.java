450153c8ccc151375965381556529f0ffcd7f095
YASH <iamyash337@gmail.com>
2023-03-23 07:55:14 +0530

    Added solution - LeetHub

---
+++ Anagram - GFG/anagram.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        boolean ans = false;
+        char s1[] = a.toCharArray();
+        char s2[] = b.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        if(Arrays.equals(s1, s2))
+            ans = true;
+        return ans;
+    }
+}
