91b47128efa2e48a70a6553509fb368c5da76b76
YASH <iamyash337@gmail.com>
2023-03-26 01:01:13 +0530

    Added solution - LeetHub

---
+++ Frogs and Jumps - GFG/frogs-and-jumps.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            
+            String inputLine1[] = br.readLine().trim().split(" "); 
+            int i=0;
+            int N = Integer.parseInt(inputLine1[i++]);
+            int leaves = Integer.parseInt(inputLine1[i++]);
+            
+            int frogs[] = new int[(int)(N)];
+            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for ( i = 0; i < N; i++) {
+                frogs[i] = Integer.parseInt(inputLine2[i]);
+            }
+        
+            Solution ob = new Solution();
+            System.out.println(ob.unvisitedLeaves(N, leaves, frogs));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        int visited[] = new int[leaves + 1];
+        int ans = 0;
+        for (int x : frogs) {
+            int frog = x;
+            if (frog <= leaves && visited[frog] == 0)
+                while (frog <= leaves) {
+                    visited[frog] = 1;
+                    frog += x;
+                }
+        }
+        for (int i = 1; i <= leaves; i++) {
+            if (visited[i] == 0)
+                ans++;
+        }
+        return ans;
+    }
+}
