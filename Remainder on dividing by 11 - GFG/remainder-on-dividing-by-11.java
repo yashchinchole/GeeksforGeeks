71412602cabfa01b5771064fef096dc3079eb269
YASH <iamyash337@gmail.com>
2023-04-03 10:27:56 +0530

    Added solution - LeetHub

---
+++ Remainder on dividing by 11 - GFG/remainder-on-dividing-by-11.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  
+        
+        int t = Integer.parseInt(br.readLine()); 
+
+        while(t > 0){
+  
+            String x = br.readLine(); 
+            
+            Solution ob = new Solution();
+		
+            System.out.println(ob.xmod11(x)); 
+
+            t--;
+        }
+    } 
+} 
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{ 
+    static int xmod11(String x)
+	{    
+	    int n = x.length();
+        int rem = 0;
+        for (int i = 0; i < n; i++) {
+            int digit = x.charAt(i) - '0';
+            rem = (rem * 10 + digit) % 11;
+        }
+        return rem;
+	}
+} 
+
