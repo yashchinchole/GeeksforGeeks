8d0830cb449f2862ac079f4a885858f7ec2001c1
YASH <iamyash337@gmail.com>
2023-04-13 20:17:44 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (89.21%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0, j = 0;
+        for (int x : pushed) {
+            pushed[i++] = x;
+            while (i > 0 && pushed[i - 1] == popped[j]) {
+                i--;
+                j++;
+            }
+        }
+        return i == 0;
+    }
+}
