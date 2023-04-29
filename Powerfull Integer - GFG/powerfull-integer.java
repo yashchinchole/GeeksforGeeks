789dcd76246723e02b839fad1ab2dc3aae1518f4
YASH <iamyash337@gmail.com>
2023-04-30 01:35:51 +0530

    Added solution - LeetHub

---
+++ Powerfull Integer - GFG/powerfull-integer.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[][]=new int[n][2];
+            for(int i=0;i<n;i++){
+                String s[]=in.readLine().trim().split(" ");
+                a[i][0]=Integer.parseInt(s[0]);
+                a[i][1]=Integer.parseInt(s[1]);
+            }
+            int k=Integer.parseInt(in.readLine().trim());
+            Solution ob=new Solution();
+            out.println(ob.powerfullInteger(n,a,k));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static int powerfullInteger(int n, int interval[][], int k) {
+        TreeMap<Integer, Integer> yash = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            yash.put(interval[i][0], yash.getOrDefault(interval[i][0], 0) + 1);
+            yash.put(interval[i][1] + 1, yash.getOrDefault(interval[i][1] + 1, 0) - 1);
+        }
+        int temp = 0;
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> e : yash.entrySet()) {
+            int key = e.getKey();
+            int value = e.getValue();
+            if (value >= 0) {
+                temp += value;
+                if (temp >= k) {
+                    ans = key;
+                }
+            } else if (value < 0) {
+                if (temp >= k) {
+                    ans = key - 1;
+                }
+                temp += value;
+            }
+        }
+        return (ans == 0 ? -1 : ans);
+    }
+}
