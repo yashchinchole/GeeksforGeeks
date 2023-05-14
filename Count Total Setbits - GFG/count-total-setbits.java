ae4dacd2cc7c99e18a4aa0b46998d57e48c6041f
YASH <iamyash337@gmail.com>
2023-05-15 00:47:44 +0530

    Added solution - LeetHub

---
+++ Count Total Setbits - GFG/count-total-setbits.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            long N;
+            N = Long.parseLong(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            long res = obj.countBits(N);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public static long countBits(long N) {
+        long ans = 0;
+        for (int i = 0; i <= N; i++)
+            ans += Integer.bitCount(i);
+        return ans;
+    }
+}
