781d80eaf9bb35499e1c8af17a2dfd0410bd13bc
YASH <iamyash337@gmail.com>
2023-05-25 19:09:08 +0530

    Added solution - LeetHub

---
+++ Expression Add Operators - GFG/expression-add-operators.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String S;
+            S = br.readLine();
+
+            int target;
+            target = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            ArrayList<String> res = obj.addOperators(S, target);
+            Collections.sort(res);
+            for (String s : res) System.out.print(s + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public static ArrayList<String> addOperators(String num, int target) {
+        ArrayList<String> rst = new ArrayList<String>();
+        if (num == null || num.length() == 0)
+            return rst;
+        helper(rst, "", num, target, 0, 0, 0);
+        return rst;
+    }
+
+    static void helper(List<String> rst, String path, String num, int target,
+            int pos, long eval, long multed) {
+        if (pos == num.length()) {
+            if (target == eval)
+                rst.add(path);
+            return;
+        }
+        for (int i = pos; i < num.length(); i++) {
+            if (i != pos && num.charAt(pos) == '0')
+                break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                helper(rst, path + cur, num, target, i + 1, cur, cur);
+            } else {
+                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur,
+                        cur);
+
+                helper(rst, path + "-" + cur, num, target, i + 1, eval - cur,
+                        -cur);
+
+                helper(rst, path + "*" + cur, num, target, i + 1,
+                        eval - multed + multed * cur, multed * cur);
+            }
+        }
+    }
+}
