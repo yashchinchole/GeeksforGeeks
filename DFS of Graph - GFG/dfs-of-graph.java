86a3aee0a3683993711554a463bc86170e33db48
YASH <iamyash337@gmail.com>
2023-08-01 07:50:49 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] v = new boolean[V];
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < V; i++)
+            if (v[i] == false)
+                dfs(V, adj, i, ans, v);
+        return ans;
+    }
+
+    public static void dfs(int V, ArrayList<ArrayList<Integer>> adj, int cur, ArrayList<Integer> ans, boolean[] v) {
+        v[cur] = true;
+        ans.add(cur);
+        for (int i : adj.get(cur))
+            if (v[i] == false)
+                dfs(V, adj, i, ans, v);
+    }
+}
